cfc6983dfa2fcc3dcf66f4c2164b38e1c90df778
rupali <36826017+rupalimo@users.noreply.github.com>
2025-10-20 15:12:22 -0400

    add boiler plate files

---
+++ code-table.js
@@ -0,0 +1,53 @@
+var SPREADSHEET_ID_AND_TAB =
+	'1W2MSTFVGcq4c3OO5rpfvUJbmktMpmMPu4M0_mgpwwgw/boycott';
+
+$(document).ready(function () {
+	$.getJSON(
+		'https://opensheet.elk.sh/' + SPREADSHEET_ID_AND_TAB,
+		function (data) {
+			console.log(data);
+
+			data.forEach(function (entry, index) {
+				console.log(entry);
+
+				if (index == 0) return;
+
+				let nav = $(
+					`<a href="#` + entry.menuItems + `">` + entry.menuItems + `</a>`
+				).appendTo('#menuItems');
+
+				let div = $(
+					`<p data-confirm="` + entry.letter + `">` + entry.letter + `</p>`
+				).appendTo('#letter');
+
+				let ul = $(
+					`<li data-confirm="` +
+						entry.signatories +
+						`">` +
+						entry.signatories +
+						`</li>`
+				).appendTo('#signatories');
+
+				let details = $(
+					`<details data-confirm="` +
+						entry.faqTitle +
+						`">
+					<summary>
+						
+						` +
+						entry.faqTitle +
+						`
+						</summary>
+						<div><p>` +
+						entry.faqSection +
+						`</p></div>
+						</details>`
+				).appendTo('#faq');
+
+				let article = $(
+					`<p data-confirm="` + entry.about + `">` + entry.about + `</p>`
+				).appendTo('#about');
+			});
+		}
+	);
+});
